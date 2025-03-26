4e9e10083628342aecc61ce2ee3248c90a95bb0b
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2025-03-26 12:43:46 -0300

    Boludeces
    Makako

--- IntroduccionJava/src/main/java/CadenaDeCaracteres/main.java
+++ IntroduccionJava/src/main/java/CadenaDeCaracteres/main.java
@@ -6,7 +6,7 @@ public class main {
 		
 		String palabra = "LinKevin";
 		
-		palabra.charAt( 5 );	// Agarra un caracter x de la palabra original
+		palabra.charAt( 5 );			// Agarra un caracter x de la palabra original
 		palabra.compareTo( "Kevin" );	// va a devolver "false" - compara el texto con la palabra orginal. Deben ser exactamente iguales
 		palabra.compareToIgnoreCase( "linkevin" );	//va a devolver "true" - compara con la palabra original pero ignora las mayúsculas y minúsculas
 		palabra.concat( "/profile" );	//agrega texto al final
@@ -18,16 +18,16 @@ public class main {
 		palabra.replace("Kevin", "Roman");	//Quedará LinRoman - Reemplaza un texto o caracter por otro
 		palabra.replace("i", "X");			//Quedará: LXnKevin - solo reemplaza la primera aparición del texto o caracter
 		palabra.replaceAll("i", "UwU");		//Quedará LUwUnKevUwUn - reemplaza todas las apariciones del texto o caracter
-		palabra.toString();			//Convierte todo a string
-		palabra.valueOf(5);			//transforma el numero en texto usando el código ASCII
-		palabra.trim();				//quita los espacios en blanco al principio y al final
-		palabra.toLowerCase();		//Convierte todo en minúsculas
-		palabra.toUpperCase();		//Convierte todo en mayúsculas
-		palabra.toCharArray();		//Convierte un string en un vector
+		palabra.toString();					//Convierte todo a string
+		palabra.valueOf(5);					//transforma el numero en texto usando el código ASCII
+		palabra.trim();						//quita los espacios en blanco al principio y al final
+		palabra.toLowerCase();				//Convierte todo en minúsculas
+		palabra.toUpperCase();				//Convierte todo en mayúsculas
+		palabra.toCharArray();				//Convierte un string en un vector
 		// ['L', 'i', 'n', 'K', 'e', 'v', 'i', 'n']
-		palabra.split("K");			//Divide la palabra original en el texto o caracter indicado.
+		palabra.split("K");					//Divide la palabra original en el texto o caracter indicado.
 		// vec[] => { "Lin", "evin" } Lin evin
-		palabra.substring( 2, 4 );	//LinKevin -> nKev
+		palabra.substring( 2, 4 );			//LinKevin -> nKev
 		
 		//conversion de Enteros a texto y viceversa
 		//Tokenized
