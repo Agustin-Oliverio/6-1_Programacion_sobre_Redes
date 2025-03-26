6e7d86e502c2bf405c643eb538cea75d9ee876cf
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2025-03-26 12:42:31 -0300

    Manejo de strings
    Comandos para el manejo de cadenas de caracteres.

---
+++ IntroduccionJava/src/main/java/CadenaDeCaracteres/main.java
@@ -0,0 +1,37 @@
+package CadenaDeCaracteres;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		String palabra = "LinKevin";
+		
+		palabra.charAt( 5 );	// Agarra un caracter x de la palabra original
+		palabra.compareTo( "Kevin" );	// va a devolver "false" - compara el texto con la palabra orginal. Deben ser exactamente iguales
+		palabra.compareToIgnoreCase( "linkevin" );	//va a devolver "true" - compara con la palabra original pero ignora las mayúsculas y minúsculas
+		palabra.concat( "/profile" );	//agrega texto al final
+		palabra.contains( "K" );		//Devuelve si existe el texto dentro de la palabra original
+		palabra.indexOf( '\n' );		//Lo mismo que charAt pero busca la ubicación de un caracter
+		palabra.indexOf( "Ke" );		//Ubicación de ese texto en la palabra palabra
+		palabra.length();				//Largo total de la cadena
+		palabra.lastIndexOf('i');		//Ultima aparición de un texto o caracter
+		palabra.replace("Kevin", "Roman");	//Quedará LinRoman - Reemplaza un texto o caracter por otro
+		palabra.replace("i", "X");			//Quedará: LXnKevin - solo reemplaza la primera aparición del texto o caracter
+		palabra.replaceAll("i", "UwU");		//Quedará LUwUnKevUwUn - reemplaza todas las apariciones del texto o caracter
+		palabra.toString();			//Convierte todo a string
+		palabra.valueOf(5);			//transforma el numero en texto usando el código ASCII
+		palabra.trim();				//quita los espacios en blanco al principio y al final
+		palabra.toLowerCase();		//Convierte todo en minúsculas
+		palabra.toUpperCase();		//Convierte todo en mayúsculas
+		palabra.toCharArray();		//Convierte un string en un vector
+		// ['L', 'i', 'n', 'K', 'e', 'v', 'i', 'n']
+		palabra.split("K");			//Divide la palabra original en el texto o caracter indicado.
+		// vec[] => { "Lin", "evin" } Lin evin
+		palabra.substring( 2, 4 );	//LinKevin -> nKev
+		
+		//conversion de Enteros a texto y viceversa
+		//Tokenized
+		
+	}
+
+}
--- IntroduccionJava/src/main/java/Introduccion/main.java
+++ IntroduccionJava/src/main/java/Introduccion/main.java
@@ -1,6 +1,8 @@
 package Introduccion;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 public class main {
@@ -16,19 +18,47 @@ public class main {
 		System.err;
 		*/
 		
-		PrintStream ps = new PrintStream(System.out);
+		/*   %d  double (numeros muy grandes)
+		 * 	 %n	 int
+		 *   %s  string
+		 *   %c  char
+		 *   %b  bool
+		 *   %f  float
+		 *   
+		 *   (float)variable   fuerza a una variable a cambiar de tipo (en ese lugar) 
+		 */
+		
+		PrintStream ps = new PrintStream(System.out);		//PrintStream es un administrador de canales de salida
 		PrintStream psErr = new PrintStream(System.err);
 		
 		ps.println("Estamos todo bien");
 		
+		
 		int linea;
-		String palabra="";
+		String palabra = "";
+		
+		int linea2;
+		String palabra2 = "";
 		try {
 			while( (linea = System.in.read()) != 13 ) {
 				palabra = palabra + (char)linea;
 				ps.println( (char)linea );
 			}
 			ps.println(palabra);
+			
+			
+			InputStreamReader isr = new InputStreamReader(System.in);	//Gestor de flujo de datos de entrada
+			BufferedReader br = new BufferedReader( isr );		//Equivalente al PrintStream
+			// BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );	
+			
+			
+			while( (linea2 = System.in.read()) != 13 ) {
+				palabra2 = palabra2 + (char)linea2;
+				ps.println( (char)linea2 );
+			}
+			ps.printf( "Con readLine:%s \n", br.readLine() );	//Lee toda la línea completa
+			
+			
 			
 		} catch (IOException e) {
 			
