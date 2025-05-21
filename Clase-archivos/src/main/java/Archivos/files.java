1935589788853132420b9f74b6a4b0a22c2e6bd9
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2025-05-21 12:42:05 -0300

    Crear archivos con metodos nuevox
    Utilizamos metodos nuevos como Printer y Buffer.

--- Clase-archivos/src/main/java/Archivos/files.java
+++ Clase-archivos/src/main/java/Archivos/files.java
@@ -1,10 +1,13 @@
 package Archivos;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,20 +20,22 @@ public class files {
 		ps = new PrintStream(System.out);
 
 		/*
-		 * // "c:\\user\\gonza\\miArchivito.txt" file = new File("miArchivito.et32");
-		 * file.createNewFile(); // Crea un archivo file.delete(); // Borra el archivo
-		 * file.deleteOnExit(); // Borra el archivo cuando termina de ejecutarse
-		 * file.exists(); // Indica si el archivo existe file.getAbsoluteFile(); //
-		 * file.getName(); // Indica el nombre del archivo file.getParent(); // Indica
-		 * en qué directorio está el archivo file.getTotalSpace(); // Indica el peso del
-		 * archivo file.isHidden(); // Indica si el archivo está oculto
-		 * file.isDirectory(); // Indica si el archivo es una carpeta file.isFile(); //
-		 * Indica si es un archivo file.list(); // Devuelve un listado de "Strings" con
-		 * los nombres de todos los archivos dentro (funciona con carpetas)
-		 * file.listFiles(); // Devuelve un listado de "Files" con los nombres de todos
-		 * los archivos dentro (funciona con carpetas) file.mkdir(); // Crea una carpeta
-		 * file.renameTo(new File("nuevoNombre.txt")); // Sirve para cambiar el nombre
-		 * del archivo (es necesario instanciar un nuevo archivo con el nuevo nombre)
+		 // "c:\\user\\gonza\\miArchivito.txt" file = new File("miArchivito.et32");
+		 file.createNewFile(); // Crea un archivo
+		 file.delete(); // Borra el archivo
+		 file.deleteOnExit(); // Borra el archivo cuando termina de ejecutarse
+		 file.exists(); // Indica si el archivo existe
+		 file.getAbsoluteFile(); //
+		 file.getName(); // Indica el nombre del archivo
+		 file.getParent(); // Indica en qué directorio está el archivo
+		 file.getTotalSpace(); // Indica el peso del archivo
+		 file.isHidden(); // Indica si el archivo está oculto
+		 file.isDirectory(); // Indica si el archivo es una carpeta
+		 file.isFile(); // Indica si es un archivo
+		 file.list(); // Devuelve un listado de "Strings" con los nombres de todos los archivos dentro (funciona con carpetas)
+		 file.listFiles(); // Devuelve un listado de "Files" con los nombres de todos los archivos dentro (funciona con carpetas)
+		 file.mkdir(); // Crea una carpeta
+		 file.renameTo(new File("nuevoNombre.txt")); // Sirve para cambiar el nombre del archivo (es necesario instanciar un nuevo archivo con el nuevo nombre)
 		 */
 
 		this.rutaFiles(file);
@@ -83,13 +88,13 @@ public class files {
 
 			fs.print("Una linea");
 			fs.println("Nueva linea");
-			fs.write('d');
-			fs.append("HOlAA");
-
-			fs.flush();
+			fs.write('d');		//escribe 1 byte
+			fs.append("HOlAA");	//es como un print, pero escribe en la posición del cursor
+			fs.flush();	//limpia todo lo que hay en el canal
+			
 		} catch (FileNotFoundException e) {
 			Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
-		} finally {
+		} finally {		//se ejecuta independientemente si hubo o no un error
 			try {
 				if (fs != null)
 					fs.close();
@@ -104,12 +109,38 @@ public class files {
 	}
 
 	public void crearFileConPrinter(File f) {
-
+		FileWriter fw = null;	// Usa buffered directo y además crea canales de comunicaciones
+		PrintWriter pw = null;	// Igual que el PrintStream pero funciona con el FileWriter
+		
+		try {
+			if(!f.exists()) {
+				try {
+					file.createNewFile();
+				} catch(IOException e) {
+					Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+				}
+				
+			}
+			
+			fw = new FileWriter(f);
+			pw = new PrintWriter(fw);
+			
+			pw.print("Una linea");
+			pw.println("Nueva linea");
+			pw.write('d');		//escribe 1 byte
+			pw.append("HOlAA");	//es como un print, pero escribe en la posición del cursor
+			pw.flush();			//limpia todo lo que hay en el canal
+			
+		} catch (FileNotFoundException e) {
+			Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+		} catch (IOException e) {
+			Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+		} finally {
+			
+		}
 	}
 
 	/**
-	 *
-	 * 
 	 * 
 	 * @param f
 	 * 
@@ -118,7 +149,32 @@ public class files {
 	 */
 
 	public void crearFileConBuffer(File f) {
-
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter(f, false);	// true = append
+			bw = new BufferedWriter(fw);
+			
+			bw.append(":3");
+			bw.write('s');
+			bw.newLine();
+			bw.flush();		//opcional por que ya es Buffered
+			
+		} catch(IOException e) {
+			Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+		} finally {
+			try {
+				if(fw != null)
+					fw.close();
+				if(bw != null)
+					bw.close();
+				
+			} catch(IOException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			}
+		}
+		
 	}
 
 	/**
