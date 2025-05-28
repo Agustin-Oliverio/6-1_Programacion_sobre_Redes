9458c703ff78d4942e8ed8fe11051ba23dc471b0
Agustin-Oliverio <107478272+Agustin-Oliverio@users.noreply.github.com>
2025-05-28 12:48:42 -0300

    finalizacion clase archivos

--- Clase-archivos/src/main/java/Archivos/files.java
+++ Clase-archivos/src/main/java/Archivos/files.java
@@ -1,9 +1,11 @@
 package Archivos;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -16,8 +18,11 @@ public class files {
 	PrintStream ps;
 	File file;
 
-	public files() {
+	public files(String ruta) {
+
 		ps = new PrintStream(System.out);
+		
+		file = new File(ruta);
 
 		/*
 		 // "c:\\user\\gonza\\miArchivito.txt" file = new File("miArchivito.et32");
@@ -38,9 +43,209 @@ public class files {
 		 file.renameTo(new File("nuevoNombre.txt")); // Sirve para cambiar el nombre del archivo (es necesario instanciar un nuevo archivo con el nuevo nombre)
 		 */
 
-		this.rutaFiles(file);
-		this.crearFileConPrintStreamEasy(file);
+		this.rutaFiles();
+		this.crearFileConPrintStreamEasy(file);		
 	}
+
+
+		
+		
+		public File rutaFiles() {
+			return this.file;
+		}
+
+		public void crearFileConPrintStreamEasy(File f) {
+			FileOutputStream fos = null;
+			PrintStream fs = null;
+
+			try {
+				fos = new FileOutputStream(f);
+				fs = new PrintStream(fos);
+
+				fs.print("Una linea");
+				fs.println("Nueva linea");
+				fs.write('d');		//escribe 1 byte
+				fs.append("HOlAA");	//es como un print, pero escribe en la posición del cursor
+				fs.flush();	//limpia todo lo que hay en el canal
+				
+			} catch (FileNotFoundException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			} finally {		//se ejecuta independientemente si hubo o no un error
+				try {
+					if (fs != null)
+						fs.close();
+
+					if (fos != null)
+						fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+		public void crearFileConPrinter(File f) {
+			FileWriter fw = null;	// Usa buffered directo y además crea canales de comunicaciones
+			PrintWriter pw = null;	// Igual que el PrintStream pero funciona con el FileWriter
+			
+			try {
+				if(!f.exists()) {
+					try {
+						file.createNewFile();
+					} catch(IOException e) {
+						Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+					}
+					
+				}
+				
+				fw = new FileWriter(f);
+				pw = new PrintWriter(fw);
+				
+				pw.print("Una linea");
+				pw.println("Nueva linea");
+				pw.write('d');		//escribe 1 byte
+				pw.append("HOlAA");	//es como un print, pero escribe en la posición del cursor
+				pw.flush();			//limpia todo lo que hay en el canal
+				
+			} catch (FileNotFoundException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			} catch (IOException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			} finally {
+				
+			}
+		}
+		
+
+		/**
+		 * 
+		 * @param f
+		 * 
+		 * @return
+		 * 
+		 */
+
+		public void crearFileConBuffer(File f, String te) {
+			BufferedWriter bw = null;
+			FileWriter fw = null;
+			
+			try {
+				fw = new FileWriter(f, false);	// true = append
+				bw = new BufferedWriter(fw);
+				
+				bw.append(":");
+				bw.write('3');
+				bw.newLine();
+				bw.flush();		//opcional por que ya es Buffered
+				
+			} catch(IOException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			} finally {
+				try {
+					if(fw != null)
+						fw.close();
+					if(bw != null)
+						bw.close();
+					
+				} catch(IOException e) {
+					Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+				}
+			}
+			
+		}
+
+		/**
+		 * 
+		 * Descripcion
+		 *
+		 * 
+		 * 
+		 * @param f un archivo al leer
+		 * 
+		 * @return Todo el texto leido.
+		 * 
+		 * @throws FileNotFoundException
+		 * 
+		 * @throws IOException
+		 * 
+		 */
+
+		public String LeerFileConBuffer(File f) throws FileNotFoundException, IOException {
+			FileReader fr = null;
+			BufferedReader br = null;
+			
+			try {
+				fr = new FileReader(f);
+				br = new BufferedReader(fr);
+				
+				String line = "", texto = "";
+				while( (line = br.readLine()) != null)
+				{
+					texto.concat( line.concat("\n") );
+				}
+				
+				return texto;
+			} catch (FileNotFoundException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			} catch (IOException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			} finally {
+				try {
+					if(fr != null)
+						fr.close();
+					if(br != null)
+						br.close();
+				} catch(IOException e) {
+					Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+				}
+
+			}
+			
+			return null;
+		}
+
+		public String leerFileCaracterAcaractet(File f) {
+			FileReader fr = null;
+			BufferedReader br = null;
+			
+			try {
+				fr = new FileReader(f);
+				
+				int caracter, EOF = -1;
+				String texto = "";
+				while( (caracter = fr.read()) != EOF ) {
+					
+					if( caracter == '\n' )
+					{
+						texto.concat("\n");
+					} else {
+						texto.concat( String.valueOf(caracter) );
+					}
+					
+				}
+				return texto;
+				
+			} catch (FileNotFoundException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			
+			} catch (IOException e) {
+				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+			
+			} finally {
+				try {
+					if(fr != null)
+						fr.close();
+					if(br != null)
+						br.close();
+				
+				} catch(IOException e) {
+					Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
+				}
+
+			}
+			return null;
+		}
+	
 
 	// ***********
 
@@ -73,134 +278,5 @@ public class files {
 	 * @author Redes-20
 	 * 
 	 */
-
-	public void rutaFiles(File f) {
-
-	}
-
-	public void crearFileConPrintStreamEasy(File f) {
-		FileOutputStream fos = null;
-		PrintStream fs = null;
-
-		try {
-			fos = new FileOutputStream(f);
-			fs = new PrintStream(fos);
-
-			fs.print("Una linea");
-			fs.println("Nueva linea");
-			fs.write('d');		//escribe 1 byte
-			fs.append("HOlAA");	//es como un print, pero escribe en la posición del cursor
-			fs.flush();	//limpia todo lo que hay en el canal
-			
-		} catch (FileNotFoundException e) {
-			Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
-		} finally {		//se ejecuta independientemente si hubo o no un error
-			try {
-				if (fs != null)
-					fs.close();
-
-				if (fos != null)
-					fos.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-	}
-
-	public void crearFileConPrinter(File f) {
-		FileWriter fw = null;	// Usa buffered directo y además crea canales de comunicaciones
-		PrintWriter pw = null;	// Igual que el PrintStream pero funciona con el FileWriter
-		
-		try {
-			if(!f.exists()) {
-				try {
-					file.createNewFile();
-				} catch(IOException e) {
-					Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
-				}
-				
-			}
-			
-			fw = new FileWriter(f);
-			pw = new PrintWriter(fw);
-			
-			pw.print("Una linea");
-			pw.println("Nueva linea");
-			pw.write('d');		//escribe 1 byte
-			pw.append("HOlAA");	//es como un print, pero escribe en la posición del cursor
-			pw.flush();			//limpia todo lo que hay en el canal
-			
-		} catch (FileNotFoundException e) {
-			Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
-		} catch (IOException e) {
-			Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
-		} finally {
-			
-		}
-	}
-
-	/**
-	 * 
-	 * @param f
-	 * 
-	 * @return
-	 * 
-	 */
-
-	public void crearFileConBuffer(File f) {
-		BufferedWriter bw = null;
-		FileWriter fw = null;
-		
-		try {
-			fw = new FileWriter(f, false);	// true = append
-			bw = new BufferedWriter(fw);
-			
-			bw.append(":3");
-			bw.write('s');
-			bw.newLine();
-			bw.flush();		//opcional por que ya es Buffered
-			
-		} catch(IOException e) {
-			Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
-		} finally {
-			try {
-				if(fw != null)
-					fw.close();
-				if(bw != null)
-					bw.close();
-				
-			} catch(IOException e) {
-				Logger.getLogger(files.class.getName()).log(Level.WARNING, null, e);
-			}
-		}
-		
-	}
-
-	/**
-	 * 
-	 * Descripcion
-	 *
-	 * 
-	 * 
-	 * @param f un archivo al leer
-	 * 
-	 * @return Todo el texto leido.
-	 * 
-	 * @throws FileNotFoundException
-	 * 
-	 * @throws IOException
-	 * 
-	 */
-
-	public String LeerFileConBuffer(File f) throws FileNotFoundException, IOException {
-
-		return null;
-
-	}
-
-	public void leerFileCaracterAcaractet(File f) {
-
-	}
 
 }
--- Clase-archivos/src/main/java/Archivos/main.java
+++ Clase-archivos/src/main/java/Archivos/main.java
@@ -1,10 +1,27 @@
 package Archivos;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PrintStream ps;
+		ps = new PrintStream(System.out);
+		
+		files arch = new files("consorti.txt");
+		
+		arch.crearFileConBuffer(arch.rutaFiles(), "Hola mundo");
+		try {
+			ps.println( arch.LeerFileConBuffer(arch.rutaFiles()) );
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
